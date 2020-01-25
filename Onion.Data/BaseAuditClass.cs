b94f83891661d18c2f0b9e3b1da3d394ac7ca833
Abdelali EL BAHLOUL <bahloul97abdel@gmail.com>
2020-01-25 19:16:03 +0100

    create our first data layer: BaseClass and firstEntity with bound the data project with repo project

---
+++ Onion.Data/BaseAuditClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Onion.Data
+{
+    public abstract class BaseAuditClass
+    {
+        public int id { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+
+    }
+}
--- Onion.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Onion.Data
-{
-    public class Class1
-    {
-    }
-}
